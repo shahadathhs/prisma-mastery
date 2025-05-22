eaa1c0a043ceafb033b2edbf89c84e25bab402fa
shahadathhs <shahadathhossensajib732@gmail.com>
2025-05-22 13:29:20 +0600

    feat: inital prisma setup completed

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,16 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 const main = async () => {
-  // const result = await prisma.post.create({
-  //     data: {
-  //         title: "This is title 2!",
-  //         content: "This is content...2",
-  //         authorName: "Fahim Ahammed Firoz"
-  //     }
-  // });
-
-  //console.log(result);
-
   const getAllFromDB = await prisma.post.findMany()
   console.log(getAllFromDB)
 }
 
 main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async e => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
