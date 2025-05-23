f0229f209e2f712ac75d6364817fd52f751dc18e
shahadathhs <shahadathhossensajib732@gmail.com>
2025-05-23 09:20:51 +0600

    chore: added loging

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
+import chalk from 'chalk'
 import { prisma } from './config/prisma.config'
 import { updatePublishStatus } from './query/update'
 
+prisma.$on('query', e => {
+  console.group(chalk.bgBlue.white.bold('📦 Prisma Query Log'))
+
+  console.log(`${chalk.yellow('🕒 Timestamp:')} ${chalk.white(e.timestamp)}`)
+  console.log(`${chalk.green('📜 Query:')} ${chalk.white(e.query)}`)
+  console.log(`${chalk.cyan('⚡ Duration:')} ${chalk.white(`${e.duration} ms`)}`)
+
+  console.groupEnd()
+  console.log(chalk.gray('-'.repeat(60)))
+})
+
 const main = async () => {
-  console.log('THIS IS QUERY OUTPUT')
   // * Here Function with query will be imported
   await updatePublishStatus(true, 1)
 }
@@ -12,7 +23,9 @@ main()
     await prisma.$disconnect()
   })
   .catch(async e => {
+    console.group(chalk.bgRed.white.bold('❌ Error'))
     console.error(e)
+    console.groupEnd()
     await prisma.$disconnect()
     process.exit(1)
   })
--- src/query/update.ts
+++ src/query/update.ts
@@ -1,10 +1,11 @@
 import { prisma } from '@/config/prisma.config'
+import util from 'util'
 
 export async function updatePublishStatus(status: boolean, id: number) {
   const post = await prisma.post.update({
     where: { id },
     data: { published: status },
   })
-  console.log('Updated Post \n', post)
+  console.log('Updated Post \n', util.inspect(post, { depth: null, colors: true }))
   return post
 }
